56932a75108f9a2de565c2caa9823eb18c75d7c5
Huỳnh Đặng Thành Trí <63769912+kaioken166@users.noreply.github.com>
2023-11-17 14:38:15 +0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// Lấy tất cả các phần tử có class 'list-group-item'
+var items = document.querySelectorAll('.list-group-item');
+
+// Gán sự kiện click cho mỗi phần tử
+items.forEach(function (item) {
+    item.addEventListener('click', function () {
+        // Xóa active class từ tất cả các phần tử
+        items.forEach(function (el) {
+            el.classList.remove('active');
+        });
+
+        // Thêm active class vào phần tử được click
+        item.classList.add('active');
+    });
+});
+
+// Lấy tất cả các phần tử có class 'nav-link'
+var links = document.querySelectorAll('.nav-link');
+
+// Gán sự kiện click cho mỗi phần tử
+links.forEach(function (link) {
+    link.addEventListener('click', function () {
+        // Xóa active class từ tất cả các phần tử
+        links.forEach(function (el) {
+            el.classList.remove('active');
+        });
+
+        // Thêm active class vào phần tử được click
+        link.classList.add('active');
+    });
+});
+
+const toggleableElements = document.getElementsByClassName('list-group-item');
+const removableElements = document.getElementById('note0')
+
+for (var i = 0; i < toggleableElements.length; i++) {
+
+    toggleableElements[i].addEventListener('click', function () {
+        console.log(1)
+        removableElements.remove()
+    });
+}
